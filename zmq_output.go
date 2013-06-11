12a2f548c2b3627ed1048c49fac007090ef5fd26
Scott Smith <scott@ohlol.net>
2013-06-11 10:12:09 -0700

    remove crap

--- zmq_output.go
+++ zmq_output.go
@@ -15,7 +15,6 @@ type ZmqOutput struct {
 func (z *ZmqOutput) connect() {
 	ctx, _ := zmq.NewContext()
 	socket, _ := ctx.NewSocket(zmq.PUSH)
-	socket.SetSndTimeout(0)
 	for _, addr := range z.Addresses {
 		log.Printf("[zmq] Connecting to: %s\n", addr)
 		socket.Connect(addr.(string))
