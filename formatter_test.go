4aa9c5593c1cec72b5e8b4a8eb43c571b71959f3
Scott Smith <scott@boundary.com>
2013-06-10 22:03:46 -0700

    tests y travis-ci

---
+++ formatter_test.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestJsonFormatter(t *testing.T) {
+	flds := make(map[string]interface{})
+	flds["fld1"] = 1
+
+	e := Event{
+		Source:     "file://path/here.log",
+		Type:       "test-event",
+		Tags:       []string{"tag1", "tag2"},
+		Fields:     flds,
+		Timestamp:  time.Now(),
+		SourceHost: "localhost",
+		SourcePath: "/path/here.log",
+		Message:    "hello world",
+	}
+	enc, err := json.Marshal(e)
+	if err != nil {
+		t.Errorf("Problem marshalling event object to JSON: %s", err)
+	}
+
+	s := JsonFormatter{}
+	if x := string(s.Format(e)); x != string(enc) {
+		t.Errorf("JsonFormatter.Format(%v) = %v, want %v", e, string(x), string(enc))
+	}
+}
+
+func TestStringFormatter(t *testing.T) {
+	e := Event{
+		Timestamp:  time.Now(),
+		SourceHost: "localhost",
+		Message:    "hello world",
+	}
+	expectedString := fmt.Sprintf("[%s] [%s] %s", e.SourceHost, e.Timestamp, e.Message)
+
+	s := StringFormatter{}
+	if x := string(s.Format(e)); x != expectedString {
+		t.Errorf("StringFormatter.Format(%v) = %v, want %v", e, x, expectedString)
+	}
+}
