0b8d27c5de777ed848d664b11476fa6643bb59a4
Scott Smith <scott@ohlol.net>
2013-10-28 01:58:57 +0000

    handle tcp connection problems

--- tcp_output.go
+++ tcp_output.go
@@ -4,40 +4,79 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"time"
 )
+
+type tcpConnection struct {
+	Host      string
+	Port      int
+	Conn      net.Conn
+	connected bool
+	timeout   time.Duration
+}
 
 type TcpOutput struct {
 	Host      string
 	Port      int
 	Formatter JsonFormatter
 	Logger    *log.Logger
-	connected bool
-	conn      *net.Conn
+	conn      tcpConnection
 }
 
-func (t *TcpOutput) connect() error {
-	log.Printf("[tcp] Connecting to: %s:%d\n", t.Host, t.Port)
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", t.Host, t.Port))
-	if err != nil {
-		log.Printf("[tcp] Could not connect to: %s:%d\n", t.Host, t.Port)
-		return err
+// defaultTimeout is the default connection timeout used by DialTimeout.
+const (
+	defaultTimeout  = 30
+	initialWaitTime = 5
+	maxWaitTime = 60
+)
+
+func (tc *tcpConnection) getConn() {
+	var (
+		err      error
+		waitTime time.Duration
+	)
+
+	connectAddr := fmt.Sprintf("%s:%d", tc.Host, tc.Port)
+
+	if tc.timeout == 0 {
+		tc.timeout = defaultTimeout * time.Second
 	}
 
-	t.conn = &conn
-	t.connected = true
-	return nil
+	waitTime = initialWaitTime
+	tc.Conn, err = net.DialTimeout("tcp", connectAddr, tc.timeout)
+	for err != nil {
+		log.Printf("[tcp] error connecting, retrying in %d seconds: %v\n", waitTime, err)
+		time.Sleep(waitTime * time.Second)
+
+		waitTime += 5
+		if waitTime > maxWaitTime {
+			waitTime = initialWaitTime
+		}
+		tc.Conn, err = net.DialTimeout("tcp", connectAddr, tc.timeout)
+	}
+}
+
+func (t *TcpOutput) connect() {
+	t.conn = tcpConnection{Host: t.Host, Port: t.Port}
+	t.conn.getConn()
+	t.conn.connected = true
 }
 
 func (t *TcpOutput) Emit(event Event) {
-	if !t.connected {
-		if err := t.connect(); err != nil {
-			return
-		}
+	var err error
+
+	if !t.conn.connected {
+		t.connect()
 	}
 
 	if event.Formatter != nil {
-		fmt.Fprintf(*t.conn, fmt.Sprintf("%s\n", string(event.Formatter(event))))
+		_, err = fmt.Fprintf(t.conn.Conn, fmt.Sprintf("%s\n", string(event.Formatter(event))))
 	} else {
-		fmt.Fprintf(*t.conn, fmt.Sprintf("%s\n", string(t.Formatter.Format(event))))
+		_, err = fmt.Fprintf(t.conn.Conn, fmt.Sprintf("%s\n", string(t.Formatter.Format(event))))
+	}
+
+	if err != nil {
+		log.Printf("[tcp] Connect issue: %v\n", err)
+		t.conn.connected = false
 	}
 }
